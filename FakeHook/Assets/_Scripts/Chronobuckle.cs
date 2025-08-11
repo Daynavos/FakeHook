944614bf892d4865aedada5603857e0d67e905f5
Daynavos <dayna22vos@gmail.com>
2025-08-11 17:23:16 +0200

    slow time logic mostly there

--- FakeHook/Assets/_Scripts/Chronobuckle.cs
+++ FakeHook/Assets/_Scripts/Chronobuckle.cs
@@ -4,10 +4,25 @@ public class Chronobuckle : MonoBehaviour
 {
     public InputActionAsset actionAsset;
 
-    private InputAction SlowTime;
-    
-    public bool timeIsSlowed;
+    private InputAction _slowTime;
 
+    private bool _resetTimer;
+
+    public enum BucklePhase
+    {
+        CanUse,
+        InUse,
+        CoolDown
+    }
+
+    [SerializeField] private BucklePhase _phaseOfBuckle;
+    
+    [SerializeField] private float activeTime;
+    [SerializeField] private float cooldownTime;
+    private float _time = 0f;
+    
+    private SpriteRenderer _spriteRenderer;
+    
     void OnEnable()
     {
         actionAsset.FindActionMap("Player").Enable();
@@ -20,35 +35,91 @@ public class Chronobuckle : MonoBehaviour
 
     void Start()
     {
-        SlowTime = actionAsset.FindAction("SlowTime");
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _slowTime = actionAsset.FindAction("Player/SlowTime");
+        _phaseOfBuckle = BucklePhase.CanUse;
     }
 
     void Update()
     {
-        if (SlowTime.WasPressedThisFrame())
+        if (_resetTimer)
         {
-            timeIsSlowed = true;
-        }
-
-        if (SlowTime.WasReleasedThisFrame())
-        {
-            timeIsSlowed = false;
+            ResetTimer();
         }
         
-        if (timeIsSlowed)
+        if (_slowTime.WasPressedThisFrame())
         {
-            Time.timeScale = 0.5f;
-            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+            if (_phaseOfBuckle == BucklePhase.CanUse)
+            {
+                _phaseOfBuckle = BucklePhase.InUse;
+            }
         }
-        else
+
+        if (_slowTime.WasReleasedThisFrame())
         {
+            _resetTimer = true;
+            if (_phaseOfBuckle == BucklePhase.InUse)
+            {
+                _phaseOfBuckle = BucklePhase.CanUse;
+            }
+
+            if (_phaseOfBuckle == BucklePhase.CoolDown)
+            {
+                return;
+            }
+        }
+        
+        if (_phaseOfBuckle == BucklePhase.CanUse)
+        {
+            _spriteRenderer.color = Color.red;
             Time.timeScale = 1f;
-            Time.fixedDeltaTime = 0.02f * Time.timeScale;
         }
+
+        if (_phaseOfBuckle == BucklePhase.InUse)
+        {
+            _spriteRenderer.color = Color.green;
+            Time.timeScale = 0.5f;
+            SlowDownActiveTimer();
+            
+        }
+
+        if (_phaseOfBuckle == BucklePhase.CoolDown)
+        {
+            _spriteRenderer.color = Color.blue;
+            Time.timeScale = 1f;
+            CooldownTimer();
+        }
+        
     }
 
     private void SlowDownActiveTimer()
     {
-        
+        _time += Time.unscaledDeltaTime;
+        if (_time > activeTime)
+        {
+            _time = 0f;
+            _phaseOfBuckle = BucklePhase.CoolDown;
+            Debug.Log("TimeOut");
+            
+        }
     }
+
+    private void CooldownTimer()
+    {
+        _time += Time.unscaledDeltaTime;
+        if (_time > cooldownTime)
+        {
+            _time = 0f;
+            _phaseOfBuckle = BucklePhase.CanUse;
+            Debug.Log("Go Again");
+            
+        }
+    }
+
+    private void ResetTimer()
+    {
+        _time = 0f;
+        _resetTimer = false;
+    }
+    
 }
