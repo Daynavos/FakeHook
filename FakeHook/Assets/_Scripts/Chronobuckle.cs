44e0c8b28aab9f0b9c3bf933c2139368cd8da5f9
Tiyani <mhlarhit@gmail.com>
2025-08-13 14:20:58 +0200

    Squashed commit of the following:
    commit 806b5dc823cf1d751fec42d568dc19f8f1233eaa
    Merge: dd9fa60 5f483e5
    Author: Tiyani <mhlarhit@gmail.com>
    Date:   Wed Aug 13 14:20:25 2025 +0200
    
        Merge branch 'tiyani' into FirstMerge
    
    commit dd9fa6050ef23e08bd29ffc2a61ceee076bc8e68
    Author: Daynavos <dayna22vos@gmail.com>
    Date:   Wed Aug 13 14:15:50 2025 +0200
    
        idk
    
    commit 6a4dc69043cd6181e1c6ba3ad3129b74cfcdf17d
    Author: Daynavos <dayna22vos@gmail.com>
    Date:   Tue Aug 12 21:41:13 2025 +0200
    
        Chronobuckle iteration 1 done
    
        1 small bug and not completely pretty but mostly there
    
    commit 944614bf892d4865aedada5603857e0d67e905f5
    Author: Daynavos <dayna22vos@gmail.com>
    Date:   Mon Aug 11 17:23:16 2025 +0200
    
        slow time logic mostly there
    
    commit 958338ab93012a4bb7ddff21d418947542d0482c
    Author: Daynavos <dayna22vos@gmail.com>
    Date:   Mon Aug 11 13:35:19 2025 +0200
    
        slow Down Started
    
    commit a30cd7339b6bc503cb014747539e2e6c8c0129a6
    Author: Daynavos <dayna22vos@gmail.com>
    Date:   Sun Aug 10 19:41:09 2025 +0200
    
        Action Asset

---
+++ FakeHook/Assets/_Scripts/Chronobuckle.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class Chronobuckle : MonoBehaviour
+{
+    public InputActionAsset actionAsset;
+
+    private InputAction _slowTime;
+    
+    private enum BucklePhase
+    {
+        FullyCharged,
+        Depleting,
+        Charging
+    }
+
+    [SerializeField] private BucklePhase phaseOfBuckle;
+    
+    [SerializeField] private float fullyChargedBuckle;
+
+    public GameObject volume;
+    public GameObject timer;
+
+    public Image timerFill;
+    
+    private float _chargeLevel = 5f;
+    private SpriteRenderer _spriteRenderer;
+    
+    void OnEnable()
+    {
+        actionAsset.FindActionMap("Player").Enable();
+    }
+
+    void OnDisable()
+    {
+        actionAsset.FindActionMap("Player").Disable();
+    }
+
+    void Start()
+    {
+        _chargeLevel = Mathf.Clamp(_chargeLevel, 0f, fullyChargedBuckle);
+        _chargeLevel = fullyChargedBuckle;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _slowTime = actionAsset.FindAction("Player/SlowTime");
+        phaseOfBuckle = BucklePhase.FullyCharged;
+    }
+
+    void Update()
+    {
+        if (_slowTime.IsPressed())
+        {
+            phaseOfBuckle = BucklePhase.Depleting;
+        }
+        else
+        {
+            if (phaseOfBuckle != BucklePhase.FullyCharged)
+            {
+                phaseOfBuckle = BucklePhase.Charging;
+            }
+        }
+        
+        if (phaseOfBuckle == BucklePhase.FullyCharged)
+        {
+            volume.SetActive(false);
+            _spriteRenderer.color = Color.red;
+            Time.timeScale = 1f;
+            timer.SetActive(false);
+        }
+        
+        if (phaseOfBuckle == BucklePhase.Depleting)
+        {
+            volume.SetActive(true);
+            _spriteRenderer.color = Color.green;
+            Time.timeScale = 0.5f;
+            DepletingTimer();
+            UpdateTimerBar();
+        }
+        
+        if (phaseOfBuckle == BucklePhase.Charging)
+        {
+            volume.SetActive(false);
+            _spriteRenderer.color = Color.blue;
+            Time.timeScale = 1f;
+            ChargingTimer();
+            UpdateTimerBar();
+        }
+    }
+
+    private void DepletingTimer()
+    {
+        UpdateTimerBar();
+        _chargeLevel -= Time.deltaTime;
+        if (_chargeLevel == 0)
+        {
+            phaseOfBuckle = BucklePhase.Charging;
+        }
+    }
+
+    private void ChargingTimer()
+    {
+        UpdateTimerBar();
+        _chargeLevel += Time.deltaTime;
+        if (_chargeLevel > fullyChargedBuckle)
+        {
+            phaseOfBuckle = BucklePhase.FullyCharged;
+        }
+    }
+
+    private void UpdateTimerBar()
+    {
+        timer.SetActive(true);
+        timerFill.fillAmount = _chargeLevel/fullyChargedBuckle;
+    }
+    
+}
