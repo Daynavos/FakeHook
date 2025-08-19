1432723815130f340363825f0fd7de6834cb61ea
Daynavos <dayna22vos@gmail.com>
2025-08-19 20:47:41 +0200

    chrono fix mostly

--- FakeHook/Assets/_Scripts/Chronobuckle.cs
+++ FakeHook/Assets/_Scripts/Chronobuckle.cs
@@ -28,7 +28,11 @@ public class Chronobuckle : MonoBehaviour
     public Image timerFill;
     
     private float _chargeLevel = 5f;
+    [SerializeField] private float cooldownDuration = 1f;
     private SpriteRenderer _spriteRenderer;
+    
+    private float _cooldownTimer = 0f; 
+    private bool _coolDown = true; 
     
     void OnEnable()
     {
@@ -46,6 +50,7 @@ public class Chronobuckle : MonoBehaviour
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _slowTime = actionAsset.FindAction("Player/SlowTime");
         phaseOfBuckle = BucklePhase.FullyCharged;
+        _cooldownTimer = cooldownDuration;
     }
 
     void Update()
@@ -86,6 +91,16 @@ public class Chronobuckle : MonoBehaviour
             ChargingTimer();
             UpdateTimerBar();
         }
+        
+        if (phaseOfBuckle == BucklePhase.CoolDown)
+        {
+            volume.SetActive(false);
+            Time.timeScale = 1f;
+            CooldownTimer();
+            ChargingTimer();   
+            UpdateTimerBar();
+        }
+
     }
 
     private void DepletingTimer()
@@ -93,16 +108,13 @@ public class Chronobuckle : MonoBehaviour
         UpdateTimerBar();
         _chargeLevel = Mathf.Clamp(_chargeLevel - Time.deltaTime, 0f, fullyChargedBuckle);
 
-        if (_chargeLevel < 0)
+        if (_chargeLevel <= 0f) 
         {
-            if (!_slowTime.IsPressed())
-            {
-                phaseOfBuckle = BucklePhase.Charging;
-            }
             Time.timeScale = 1f;
             phaseOfBuckle = BucklePhase.CoolDown;
         }
     }
+
 
     private void ChargingTimer()
     {
@@ -119,6 +131,17 @@ public class Chronobuckle : MonoBehaviour
     {
         timer.SetActive(true);
         timerFill.fillAmount = _chargeLevel/fullyChargedBuckle;
+    }
+    
+    private void CooldownTimer()
+    {
+        _cooldownTimer -= Time.deltaTime;
+        UpdateTimerBar();
+
+        if (_cooldownTimer <= 0f)
+        {
+            phaseOfBuckle = BucklePhase.Charging;
+        }
     }
     
 }
